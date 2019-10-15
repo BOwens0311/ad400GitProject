f0abd2db341140ad1e8439bdc819ad713fa9b765
BOwens0311 <56457140+BOwens0311@users.noreply.github.com>
2019-10-14 21:49:15 -0700

    HelloGitHub.java
    A program to say hello to GitHub

---
+++ HelloGitHub/src/hellogithub/HelloGitHub.java
@@ -0,0 +1,16 @@
+
+package hellogithub;
+
+/**
+ * An application to demonstrate the utility of git repos
+ * @author owens
+ */
+public class HelloGitHub {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello All");
+        String branchName = "Master";
+        System.out.println("This is the " + branchName + " Branch");
+    }
+    
+}
